a4b61a8d499fef88d722adeb675f1d08d8032ae5
Charles-Dubois <charles.dubois.h@gmail.com>
2022-02-01 14:04:08 +0100

    1st step

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { name: "", capital: "", flag: "", populaion: "", region: "" };
+  }
+
+  componentDidMount() {
+    fetch("https://restcountries.com/v3.1/name/france")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          name: res[0].name.common,
+          capital: res[0].capital[0],
+          flag: res[0].flags.png,
+          population: res[0].population,
+          region: res[0].region,
+        });
+        console.log(this.state.capital);
+      });
+  }
+  render() {
+    return (
+      <>
+        <p>Hello wolrd</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pays: {this.state.name} <br />
+          Capital: {this.state.capital} <br />
+          Drapeau:{" "}
+          <img
+            src={this.state.flag}
+            alt={`Drapeau de ${this.state.name}`}
+          />{" "}
+          <br />
+          Population:{this.state.population}
+          <br />
+          Region: {this.state.region}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </>
+    );
+  }
 }
-
 export default App;

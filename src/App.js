9c53871e21cfbd25964f9da6de6435ddedefce8a
Charles-Dubois <charles.dubois.h@gmail.com>
2022-02-01 14:43:40 +0100

    étape 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Button from "./components/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { name: "", capital: "", flag: "", populaion: "", region: "" };
+    this.state = {
+      loading: true,
+      name: "france",
+      capital: "",
+      flag: "",
+      populaion: "",
+      region: "",
+    };
+    this.changeCountryBrazil = this.changeCountryBrazil.bind(this);
+    this.changeCountryFrance = this.changeCountryFrance.bind(this);
+    this.changeCountryCroatia = this.changeCountryCroatia.bind(this);
+    this.getCountry = this.getCountry.bind(this);
   }
 
   componentDidMount() {
@@ -19,28 +30,62 @@ class App extends React.Component {
           population: res[0].population,
           region: res[0].region,
         });
-        console.log(this.state.capital);
       });
   }
+
+  getCountry(country) {
+    fetch("https://restcountries.com/v3.1/name/" + this.state.name)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          name: res[0].name.common,
+          capital: res[0].capital[0],
+          flag: res[0].flags.svg,
+          population: res[0].population,
+          region: res[0].region,
+        });
+      });
+    console.log(this.state.capital);
+  }
+  changeCountryFrance(country) {
+    this.setState({ name: country });
+    this.getCountry(country);
+  }
+  changeCountryBrazil(country) {
+    this.setState({ name: country });
+    this.getCountry(country);
+  }
+  changeCountryCroatia(country) {
+    this.setState({ name: country });
+    this.getCountry(country);
+  }
+
   render() {
-    return (
-      <>
-        <p>Hello wolrd</p>
-        <p>
-          Pays: {this.state.name} <br />
-          Capital: {this.state.capital} <br />
-          Drapeau:{" "}
-          <img
-            src={this.state.flag}
-            alt={`Drapeau de ${this.state.name}`}
-          />{" "}
-          <br />
-          Population:{this.state.population}
-          <br />
-          Region: {this.state.region}
-        </p>
-      </>
-    );
+    if (this.state.loading === false) {
+      return (
+        <>
+          <Button onClick={this.changeCountryFrance}>France</Button>
+          <Button onClick={this.changeCountryBrazil}>Brazil</Button>
+          <Button onClick={this.changeCountryCroatia}>Croatia</Button>
+          <p>
+            Pays: {this.state.name} <br />
+            Capital: {this.state.capital} <br />
+            Drapeau:{" "}
+            <img
+              src={this.state.flag}
+              alt={`Drapeau de ${this.state.name}`}
+            />{" "}
+            <br />
+            Population:{this.state.population}
+            <br />
+            Region: {this.state.region}
+          </p>
+        </>
+      );
+    } else if (this.state.loading === true) {
+      return <h1>Loading</h1>;
+    }
   }
 }
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <>
+        <button
+          onClick={() => this.props.onClick(this.props.children.toLowerCase())}
+        >
+          {this.props.children}
+        </button>
+      </>
+    );
+  }
+}
+export default Button;
